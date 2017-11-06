7f67827e934ba98df17a759229cc21ebb6590bab
shift <ivanlampert@hotmail.com>
2017-11-06 20:36:11 -0200

    Removido validações.

--- src/main/java/com/univates/tcc/abacate/aplicacao/rest/pesquisa/FabricaDePageRequest.java
+++ src/main/java/com/univates/tcc/abacate/aplicacao/rest/pesquisa/FabricaDePageRequest.java
@@ -3,9 +3,6 @@ package com.univates.tcc.abacate.aplicacao.rest.pesquisa;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.util.StringUtils;
-
-import com.univates.tcc.abacate.dominio.excecoes.ValoresInvalidos;
 
 public class FabricaDePageRequest {
 
@@ -13,10 +10,6 @@ public class FabricaDePageRequest {
 	}
 
 	public static PageRequest criar(Integer pagina, Integer quantidade, String atributoOrdenado, String ordem) {
-		validarParametros(pagina, quantidade);
-		if (naoPossuiOrdenacao(atributoOrdenado))
-			return new PageRequest(pagina, quantidade);
-			
 		pagina = pagina == null ? 0 : pagina;
 		quantidade = quantidade == null ? Integer.MAX_VALUE : quantidade;
 		
@@ -26,13 +19,4 @@ public class FabricaDePageRequest {
 		return new PageRequest(pagina, quantidade, new Sort(Direction.fromString(ordem), atributoOrdenado));
 	}
 
-	private static boolean naoPossuiOrdenacao(String atributoOrdenado) {
-		return StringUtils.isEmpty(atributoOrdenado);
-	}
-
-	private static void validarParametros(Integer pagina, Integer quantidade) {
-		if (pagina == null || quantidade == null)
-			throw new ValoresInvalidos("Página e Quantidade são valoroes requeridos paginação");
-	}
-	
 }
